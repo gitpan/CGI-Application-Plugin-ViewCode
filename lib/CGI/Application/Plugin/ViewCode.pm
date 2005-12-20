6a44d051ec646b9c0632b5bc3be123b772b48b08
Michael Peters <wonko@cpan.org>
2005-12-20 16:37:47 +0000

    Import of WONKO/CGI-Application-Plugin-ViewCode-1.00 from CPAN.
    gitpan-cpan-distribution: CGI-Application-Plugin-ViewCode
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         WONKO/CGI-Application-Plugin-ViewCode-1.00.tar.gz
    gitpan-cpan-author:       WONKO
    gitpan-cpan-maturity:     released

--- lib/CGI/Application/Plugin/ViewCode.pm
+++ lib/CGI/Application/Plugin/ViewCode.pm
@@ -1,7 +1,6 @@
 package CGI::Application::Plugin::ViewCode;
 use warnings;
 use strict;
-use Syntax::Highlight::Perl::Improved;
 
 =head1 NAME
 
@@ -9,7 +8,7 @@ CGI::Application::Plugin::ViewCode - View the source of the running application
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '1.00';
 
 # DEFAULT_STYLES taken from Apache::Syntax::Highlight::Perl by Enrico Sorcinelli
 our %DEFAULT_STYLES = (
@@ -51,17 +50,24 @@ In your CGI::Application based class
 
     use CGI::Application::Plugin::ViewCode;
 
-Then you can view your modules source as it's running by changing the url
+Then you can view your module's source (or pod) as it's running by changing the url
 
     ?rm=view_code
     ?rm=view_code&line_no=1
+    ?rm=view_code&pod=0&line_no=0
     ?rm=view_code&module=CGI-Application
+
+    ?rm=view_pod
+    ?rm=view_pod&module=CGI-Application
 
 =head1 INTERFACE
 
-This plugin works by adding an extra run mode named C<< view_code >> to the
-application. By calling this run mode you can see the source of the running module
+This plugin works by adding extra run modes (named C<view_code> and C< view_pod >) to the
+application. By calling this run mode you can see the source or POD of the running module
 (by default) or you can specify which module you would like to view (see L<SECURITY>).
+
+
+=head2 view_code
 
 This extra run mode will accept the following arguments in the query string:
 
@@ -76,29 +82,86 @@ since colons (':') aren't simply typed into URL's, you can just substitute '-' f
 
 =item highlight
 
-Turn syntax highlighting (using L<Syntax::Highlight::Perl::Improved>) on or off. 
-By default it is 1 (on).
+Boolean indicates whether syntax highlighting (using L<Syntax::Highlight::Perl::Improved>) 
+is C<on> or C<off>. By default it is C<on>.
 
 =item line_no
 
-Turn viewing of line numbers on or off. By default it is 1 (on).
+Boolean indicates whether the viewing of line numbers is C<on> or C<off>. By default it is C<on>.
 
 =item pod
 
-Turn viewing of pod on or off. By default it is 1 (on).
+Boolean indicates whether POD is seen or not. By default it is seen>.
 
 =back
+
+=head2 view_pod
+
+This extra run mode will accept the following arguments in the query string:
+
+=over
+
+=item module
+
+The name of the module to view. By default it is the module currently being run. Also,
+since colons (':') aren't simply typed into URL's, you can just substitute '-' for '::'.
+
+    ?rm=view_pod?module=My-Base-Class
+
+=back
+
+=head1 AS A POPUP WINDOW
+
+This plugin can be used in conjunction with L<CGI::Application::Plugin::DevPopup>. If we detect
+that L<CGI::Application::Plugin::DevPopup> is running and turned on, we will create a sub-report
+that includes the highlighted source code.
+
+
+So you can simply do the following:
+
+    BEGIN { $ENV{CAP_DEVPOPUP_EXEC} = 1; } # turn it on for real
+    use CGI::Application::Plugin::DevPopup;
+    use CGI::Application::Plugin::ViewCode;
+
+Befault, this report will be the same thing produced by C<view_code>. If you want this
+report to include the C<view_pod> report, simply set the the C<$ENV{CAP_VIEWCODE_POPUP_POD}>
+to true. You can also turn off the C<view_code> report but setting 
+C<$ENV{CAP_VIEWCODE_POPUP_CODE}> to false.
+
+    # have the POD report, but not the code in the dev popup window
+    BEGIN { 
+        $ENV{CAP_DEVPOPUP_EXEC} = 1;       # turn it on for real
+        $ENV{CAP_VIEWCODE_POPUP_POD} = 1;  # turn on POD report
+        $ENV{CAP_VIEWCODE_POPUP_CODE} = 0; # turn off code report
+    }
+    use CGI::Application::Plugin::DevPopup;
+    use CGI::Application::Plugin::ViewCode;
 
 =cut
 
 sub import {
     my $caller = scalar(caller);
     $caller->add_callback( init => \&_add_runmode );
+
+    # if we are running under CGI::Application::Plugin::DevPopup
+    if( $ENV{CAP_DEVPOPUP_EXEC} ) {
+        # if we wan't to add the POD report
+        if( exists $ENV{CAP_VIEWCODE_POPUP_POD} && $ENV{CAP_VIEWCODE_POPUP_POD} ) {
+            $caller->add_callback( devpopup_report => \&_view_pod );
+        }
+        # include the view_code report by default unless it's turned off
+        if(! (exists $ENV{CAP_VIEWCODE_POPUP_CODE} && !$ENV{CAP_VIEWCODE_POPUP_CODE}) ) {
+            $caller->add_callback( devpopup_report => \&_view_code );
+        }
+    }
 }
 
 sub _add_runmode {
     my $self = shift;
-    $self->run_modes( view_code => \&_view_code );
+    $self->run_modes( 
+        view_code => \&_view_code,
+        view_pod  => \&_view_pod
+    );
 }
 
 sub _view_code {
@@ -114,17 +177,10 @@ sub _view_code {
         }
     }
         
-    # defaults
-    $options{module} = $query->param('module') || ref($self);
-
     # get the file to be viewed
-    my $module = $options{module};
+    my $module = _module_name($query->param('module') || ref($self));
     # change into file name
-    my $module_file = $module;
-    $module_file =~ s/-|(::)/\//g;    # allow for :: or -
-    $module_file .= '.pm';
-
-    my $file = $INC{$module_file};
+    my $file = _module_file_name($module);
 
     # make sure the file exists
     if( $file && -e $file ) {
@@ -144,6 +200,7 @@ sub _view_code {
         }
 
         # now use Syntax::Highlight::Perl::Improved to do the work
+        require Syntax::Highlight::Perl::Improved;
         my $formatter = Syntax::Highlight::Perl::Improved->new();
         $formatter->define_substitution(%SUBSTITUTIONS);
         foreach my $style (keys %styles) {
@@ -178,7 +235,7 @@ sub _view_code {
                         $link =~ s/::/-/g;
                         my $rm = $self->mode_param();
                         $rm = ref $rm ? 'rm' : $rm; # not really anything we can do if their mode_param returns a sub ref
-                        $link = "?$rm=view_code&amp;module=$package";
+                        $link = "?$rm=view_code&amp;module=$package;view_code_no_popup=1";
                         $line =~ s/<span class="Package">[^<]*<\/span>/<a class="Package" href="$link">$package<\/a>/;
                     }    
                 }
@@ -186,21 +243,92 @@ sub _view_code {
         }
         my $code = join('', @lines);
 
-        return qq(
-        <html>
-        <head>
-            <title>$module - View Source</title>
-            <style>$style_sec</style>
-        </head>
-        <body>
-            <pre>$code</pre>
-        </body>
-        </html>
-        );
+        # if we are under CGI::Application::Plugin::DevPopup then let's create this as a report instead
+        if( $ENV{CAP_DEVPOPUP_EXEC} && !$query->param('view_code_no_popup') ) {
+            $self->devpopup->add_report(
+                title   => 'View Code',
+                summary => "View code of $module", 
+                report  => "<style>$style_sec</style><pre>$code</pre>",
+            );
+        } else {
+            return qq(
+            <html>
+            <head>
+                <title>$module - View Source</title>
+                <style>$style_sec</style>
+            </head>
+            <body>
+                <pre>$code</pre>
+            </body>
+            </html>
+            );
+        }
     } else {
         return _error( ($file ? "File $file " : "Module $module ") . "does not exist!");
     }
 }
+
+sub _view_pod {
+    my $self = shift;
+    my $query = $self->query;
+
+    # get the file to be viewed
+    my $module = _module_name($query->param('module') || ref($self));
+    # change into file name
+    my $file = _module_file_name($module);
+
+    # make sure the file exists
+    if( $file && -e $file ) {
+        require Pod::Xhtml;
+        my $pod_parser = new Pod::Xhtml(
+            StringMode   => 1,
+            MakeIndex    => 0,
+            FragmentOnly => 1,
+            TopLinks     => 0,
+            MakeMeta     => 0,
+        );
+        $pod_parser->parse_from_file($file);
+        my $pod = $pod_parser->asString;
+
+        # if we are under CGI::Application::Plugin::DevPopup then let's create this as a report instead
+        if( $ENV{CAP_DEVPOPUP_EXEC} && !$query->param('view_code_no_popup') ) {
+            $self->devpopup->add_report(
+                title   => 'View POD',
+                summary => "View POD of $module", 
+                report  => "<pre>$pod</pre>",
+            );
+        } else {
+            return qq(
+            <html>
+            <head>
+                <title>$module - View POD</title>
+            </head>
+            <body>
+                <pre>$pod</pre>
+            </body>
+            </html>
+            );
+        }
+    } else {
+        return _error( ($file ? "File $file " : "Module $module ") . "does not exist!");
+    }
+}
+
+
+sub _module_name {
+    my $name = shift;
+    $name =~ s/-/::/g;  
+    return $name;
+}
+
+sub _module_file_name {
+    my $module = shift;
+    # change into file name
+    $module =~ s/::/\//g;
+    $module .= '.pm';
+    return $INC{$module};
+}
+
 
 sub _error {
     my $message = shift;
